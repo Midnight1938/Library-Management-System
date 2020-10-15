109671a8c5b38b0bfd2ce23c28495f18747f83ff
Midnight1938 <dhoomsaksham2004@gmail.com>
2020-10-15 20:37:11 +0530

    Connected To DB, added way to add

--- index.py
+++ index.py
@@ -18,17 +18,128 @@ class MainApp(QMainWindow, ui):
 
     def Handle_UI_Changes(self):
         self.Hiding_Theme()
+        self.tabWidget.tabBar().setVisible(False)
 
 
     def Handle_Buttons(self):
+    ## ** For Themes ** ##
         self.pushButton_5.clicked.connect(self.Show_Theme)
         self.pushButton_21.clicked.connect(self.Hiding_Theme)
-
+    ## ** For Navigation ** ##
+        self.pushButton.clicked.connect(self.Open_Daily_Manage)
+        self.pushButton_2.clicked.connect(self.Open_Books)
+        self.pushButton_3.clicked.connect(self.Open_Users)
+        self.pushButton_4.clicked.connect(self.Open_Tweaks)
+    ## ** For Books ** ##
+        self.pushButton_7.clicked.connect(self.Add_New_Book)
+    ## ** Adding Categories ** ##
+        self.pushButton_14.clicked.connect(self.Add_Category)
+    ## ** Adding Author ** ##
+        self.pushButton_15.clicked.connect(self.Add_Author)
+    ## ** Adding Publisher ** ##
+        self.pushButton_16.clicked.connect(self.Add_Publisher)
+    
+####### ** ---------------- ** #######
+####### **  Theme Tweaking  ** #######
+####### ** ---------------- ** #######
     def Show_Theme(self):
         self.groupBox_3.show()
 
     def Hiding_Theme(self):
         self.groupBox_3.hide()
+
+####### ** ---------------- ** #######
+####### ** Tabs to navigate ** #######
+####### ** ---------------- ** #######
+    def Open_Daily_Manage(self):
+        self.tabWidget.setCurrentIndex(0)
+    
+    def Open_Books(self):
+        pass
+        self.tabWidget.setCurrentIndex(1)
+    
+    def Open_Users(self):
+        self.tabWidget.setCurrentIndex(2)
+
+    def Open_Tweaks(self):
+        pass
+        self.tabWidget.setCurrentIndex(3)
+
+####### ** ---------------- ** #######
+####### **   Books Stuff    ** #######
+####### ** ---------------- ** #######
+    def Add_New_Book(self):
+        self.db = pymysql.connect(host='localhost' , user='root' , password='Password123#@' , db='Library')
+        self.cur = self.db.cursor()
+        
+        book.title = self.lineEdit_3.text()
+        book.code = self.lineEdit_4.text()
+        book.category = self.comboBox_3.CurrentText()
+        book.author = self.comboBox_4.CurrentText()
+        book.publisher = self.comboBox_5.CurrentText()
+        book_price = self.lineEdit_5.text()
+    
+    def Saerch_Book(self):
+        pass
+    
+    def Edit_Book(self):
+        pass
+    
+    def Remove_Book(self):
+        pass
+    
+####### ** ---------------- ** #######
+####### **   Users Stuff    ** #######
+####### ** ---------------- ** #######
+    def Add_User(self):
+        pass
+
+    def Login_User(self):
+        pass
+
+    def Edit_User_Info(self):
+        pass
+
+####### ** ---------------- ** #######
+####### **   Tweaks Stuff   ** #######
+####### ** ---------------- ** #######
+
+    def Add_Category(self):
+        
+        self.db = pymysql.connect(host='localhost' , user='root' , password='Password123#@' , db='Library')
+        self.cur = self.db.cursor()
+        
+        category_name = self.lineEdit_19.text()
+        
+        self.cur.execute('''
+                         INSERT INTO Category (Category_name) VALUES (%s)
+                         ''' , (category_name,))
+        self.db.commit()
+        self.statusBar().showMessage("Category added Succesfully")
+        
+    def Add_Author(self):
+        self.db = pymysql.connect(host='localhost' , user='root' , password='Password123#@' , db='Library')
+        self.cur = self.db.cursor()
+        
+        author_name = self.lineEdit_20.text()
+        
+        self.cur.execute('''
+                         INSERT INTO Author (Author_name) VALUES (%s)
+                         ''' , (author_name,))
+        self.db.commit()
+        self.statusBar().showMessage("Author Added Succesfully")
+
+    def Add_Publisher(self):
+        self.db = pymysql.connect(host='localhost' , user='root' , password='Password123#@' , db='Library')
+        self.cur = self.db.cursor()
+        
+        publisher_name = self.lineEdit_21.text()
+        
+        self.cur.execute('''
+                         INSERT INTO Publisher (Publisher_name) VALUES (%s)
+                         ''' , (publisher_name,))
+        self.db.commit()
+        self.statusBar().showMessage("Publisher added Succesfully")
 
 def main():
     app = QApplication(sys.argv)
