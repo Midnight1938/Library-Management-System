fa6c6e313759fde9b27b6f02283cedd5ee45d3e4
Midnight1938 <dhoomsaksham2004@gmail.com>
2020-10-15 16:31:45 +0530

    Aminated Themes Panel

--- index.py
+++ index.py
@@ -2,6 +2,8 @@ from PyQt5.QtCore import *
 from PyQt5.QtGui import *
 from PyQt5.QtWidgets import *
 import sys
+import pymysql
+pymysql.install_as_MySQLdb()
 from PyQt5.uic import loadUiType
 
 ui, _ = loadUiType('Library.ui')
@@ -11,7 +13,22 @@ class MainApp(QMainWindow, ui):
     def __init__(self):
         QMainWindow.__init__(self)
         self.setupUi(self)
+        self.Handle_UI_Changes()
+        self.Handle_Buttons()
 
+    def Handle_UI_Changes(self):
+        self.Hiding_Theme()
+
+
+    def Handle_Buttons(self):
+        self.pushButton_5.clicked.connect(self.Show_Theme)
+        self.pushButton_21.clicked.connect(self.Hiding_Theme)
+
+    def Show_Theme(self):
+        self.groupBox_3.show()
+
+    def Hiding_Theme(self):
+        self.groupBox_3.hide()
 
 def main():
     app = QApplication(sys.argv)
